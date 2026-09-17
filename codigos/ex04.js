6c57c830fc73a2f5d94741b30d18579101161323
ale0217 <juka.6635@gmail.com>
2026-09-16 23:28:41 -0300

    adicionar ex04

---
+++ codigos/ex04.js
@@ -0,0 +1,8 @@
+//8 undefined, ele vai imprimir o dobro do valor passado, mas não vai retornar nada, por isso a variavel a vai ser undefined
+
+function dobro  (n) {
+    console.log(n * 2);
+};
+
+const a = dobro(4);
+console.log(a);
