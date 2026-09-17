5e9a2365ec03b5739b00a536ddc9dc3feb356449
ale0217 <juka.6635@gmail.com>
2026-09-16 22:30:42 -0300

    aula-01/

---
+++ codigos/ex01.js
@@ -0,0 +1,3 @@
+console.log( "Alexandre Rosa Junior")
+console.log( "Campinas - SP")
+console.log( "Construir uma carreira de sucesso em tecnologia é o meu objetivo principal, e estou determinado a alcançar esse objetivo com dedicação e aprendizado contínuo.")
