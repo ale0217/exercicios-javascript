26cdcc7148fb9cd060a2dd6e84b05f100aae7a89
ale0217 <juka.6635@gmail.com>
2026-09-16 22:34:40 -0300

    Delete codigos directory

--- codigos/ex01.js
+++
@@ -1,3 +0,0 @@
-console.log( "Alexandre Rosa Junior")
-console.log( "Campinas - SP")
-console.log( "Construir uma carreira de sucesso em tecnologia é o meu objetivo principal, e estou determinado a alcançar esse objetivo com dedicação e aprendizado contínuo.")
